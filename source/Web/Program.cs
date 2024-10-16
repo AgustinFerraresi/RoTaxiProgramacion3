52122ab5f8db008e75e6bf3321b5d970df44a3e1
ivanapanowicz <ivanapanowicz@gmail.com>
2024-10-16 16:48:23 -0300

    Autenticacion

--- source/Web/Program.cs
+++ source/Web/Program.cs
@@ -1,9 +1,14 @@
+using System.Text;
 using Application.Interfaces;
 using Application.Services;
 using Domain.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using static Infrastructure.Services.AuthenticationService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,18 +17,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IPassengerRepository, PassengerRepository>();
-builder.Services.AddScoped<IPassangerService, PassengerService>();
 
-builder.Services.AddScoped<IDriverRepository, DriverRepository>();
-builder.Services.AddScoped<IDriverService, DriverService>();
 
-builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
-builder.Services.AddScoped<IVehicleService, VehicleService>();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    setupAction.AddSecurityDefinition("RoTaxiApiBearerAuth", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Acá pegar el token generado al loguearse."
+    });
 
-builder.Services.AddScoped<IRideRepository, RideRepository>();
-builder.Services.AddScoped<IRideService, RideService>();
+    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "RoTaxiApiBearerAuth" }
+                }, new List<string>() }
+    });
+});
 
 var connection = new SqliteConnection("Data Source = DB-RoTaxiApp.db");
 connection.Open();
@@ -34,8 +50,42 @@ using (var command = connection.CreateCommand())
     command.ExecuteNonQuery();
 }
 
-builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlite(connection, b => b.MigrationsAssembly("Infrastructure")));
-//builder.Services.AddDbContext<ApplicationContext>(DbContext => DbContext.UseSqlite("Data Source=TengoQueBorrarEsto"));
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlite(connection));
+
+
+var authOptions = builder.Configuration.GetSection("AutenticacionService").Get<AutenticacionServiceOptions>();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["AutenticacionService:Issuer"],
+            ValidAudience = builder.Configuration["AutenticacionService:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["AutenticacionService:SecretForKey"]))
+        };
+    }
+);
+
+#region Repositories
+builder.Services.AddScoped<IPassengerRepository, PassengerRepository>();
+builder.Services.AddScoped<IDriverRepository, DriverRepository>();
+builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
+builder.Services.AddScoped<IRideRepository, RideRepository>();
+#endregion
+
+#region Services
+builder.Services.Configure<AutenticacionServiceOptions>(
+    builder.Configuration.GetSection(AutenticacionServiceOptions.AutenticacionService));
+builder.Services.AddScoped<IAuthenticateService, AuthenticationService>();
+builder.Services.AddScoped<IPassangerService, PassengerService>();
+builder.Services.AddScoped<IDriverService, DriverService>();
+builder.Services.AddScoped<IVehicleService, VehicleService>();
+builder.Services.AddScoped<IRideService, RideService>();
+#endregion
 
 var app = builder.Build();
 
